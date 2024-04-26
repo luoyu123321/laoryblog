bb41cf01db324698060f94fad4bb0be02753b613
luoyu123321 <779231460@qq.com>
2024-04-26 11:26:18 +0800

    导航适配

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,23 +1,59 @@
 
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
-import { Flex } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { Flex, Dropdown, Space } from 'antd';
 
 interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ }) => {
+
+  const [isInit, setIsInit] = useState<boolean>(true)
+  const [showNbr, setShowNbr] = useState<number>(0)
+  const [items, setItems] = useState<any[]>(navList.map((item, index) => {
+    return {
+      key: index,
+      label: <Link href={item.url} prefetch={true} >{item.name}</Link>,
+      url: item.url
+    }
+  }))
+
   const pathname = usePathname()
+
+  useEffect(() => {
+    const width = window.innerWidth;
+    const nbr =
+      width < 300 ? 0 :
+        width < 400 ? 1 :
+          width < 500 ? 2 :
+            width < 600 ? 4 : 10;
+    setShowNbr(nbr)
+    setItems(items.slice(nbr, 100))
+    setIsInit(false)
+  }, [])
+
 
   return (
     <Flex gap="middle" vertical>
-      <Flex justify='flex-end' align='center' className='header-nav' >
-        <div style={baseStyle} ><Link href={'/'}><span className={pathname === '/' ? 'header-nav-active' : ''}>首页</span></Link></div>
-        {navList.map((item, index) =>
-          <div key={index} style={baseStyle} ><Link href={item.url} prefetch={true} ><span className={pathname.startsWith(item.url) ? 'header-nav-active' : ''}> {item.name}</span></Link></div>)
+      <Flex justify='center' wrap="wrap" gap="40px" align='center' className='header-nav' >
+        <div><Link href={'/'}><span className={pathname === '/' ? 'header-nav-active' : ''}>首页</span></Link></div>
+        {navList.slice(0, showNbr).map((item, index) =>
+          <div key={index}><Link href={item.url} prefetch={true} ><span className={pathname.startsWith(item.url) ? 'header-nav-active' : ''}> {item.name}</span></Link></div>)
         }
+
+        {(showNbr < 10 && !isInit) && <Dropdown menu={{ items }}>
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              <span style={{ fontSize: '16px' }} className={items.some(item => pathname.startsWith(item.url)) ? 'header-nav-active' : ''}>
+                更多
+              </span>
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>}
       </Flex>
     </Flex>
   );
@@ -44,9 +80,3 @@ const navList = [{
   name: '请求测试',
   url: '/requesttest'
 }];
-
-const baseStyle: React.CSSProperties = {
-  width: 'fit-content',
-  marginRight: '1.5rem',
-  height: '100%',
-};
