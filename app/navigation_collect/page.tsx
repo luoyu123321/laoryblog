8230dc70fd695b4a8978c0bd47a5728efe76a653
luoyu123321 <779231460@qq.com>
2024-05-06 22:39:25 +0800

    删除返回按钮

--- app/navigation_collect/page.tsx
+++ app/navigation_collect/page.tsx
@@ -13,7 +13,8 @@ function NavigationCollect({ }) {
         src="https://homepage-navigation.laory.cn"
       // sandbox="allow-same-origin allow-scripts"  // 会禁止里面url跳转
       ></iframe>
-      <span className='nav-container-back-btn' onClick={() => { window.history.back() }}> 返回上一页 </span>
+      {/* 悬浮的返回按钮，页面优化不需要了，后期需要可以重新添加，记得样式也要取消注释 */}
+      {/* <span className='nav-container-back-btn' onClick={() => { window.history.back() }}> 返回上一页 </span> */}
     </div >
   );
 }
