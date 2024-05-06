ceab77fc46b8e65ff9d2418b40beeeae11b6caa1
luoyu123321 <779231460@qq.com>
2024-05-06 19:50:00 +0800

    调背景透明度

--- app/navigation_collect/page.tsx
+++ app/navigation_collect/page.tsx
@@ -10,7 +10,7 @@ function NavigationCollect({ }) {
         name='NavigationCollect'
         width={'100%'}
         height={'99%'}
-        src="https://robin901118.gitee.io/homepage_navigation/"
+        src="https://homepage-navigation.laory.cn"
       // sandbox="allow-same-origin allow-scripts"  // 会禁止里面url跳转
       ></iframe>
       <span className='nav-container-back-btn' onClick={() => { window.history.back() }}> 返回上一页 </span>
