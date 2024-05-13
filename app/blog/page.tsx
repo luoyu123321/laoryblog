8f6c8118e1bbd2c2ee9c2f15b012f06d74764262
luoyu123321 <779231460@qq.com>
2024-05-14 00:06:59 +0800

    blog更新

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,46 @@
 'use client';
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react';
+import Image from 'next/image'
+
 
 interface blogProps {
 }
 
 const Blog: FC<blogProps> = ({ }) => {
-  
+
   return (
-    <div className='blog' style={{color: '#fff'}}>
-      <div> 敬请期待</div>
-      <p>1、搭建next项目的博客编写</p>
-      <p>2、首页展示大模型对话框试用(已完成)</p>
-      <p>3、小工具待完善，显示各种工具的展示，点开进入详情，gpt也可以放在小工具（已完成）</p>
-      <p>4、tailwind使用学习--(小完成，放弃使用)</p>
-      <p>5、抽奖待完善----（已完成）</p>
-      <p>6、预加载优化待学习完善（主要是服务端渲染，请求服务端处理）</p>
-      <p>7、缓存敏感数据处理问题---（已完成）</p>
+    <div className='blog' style={{ color: '#fff' }}>
+      <div className='blog-body'>
+        <div className='blog-body-title' style={{ borderBottom: "3px solid #000" }}>
+          最新发布
+        </div>
+        <div className='blog-body-content'>
+          <a className='blog-body-content-box-bg' href='https://juejin.cn/post/7367552374335176755' target="_blank" >
+            <div className='blog-body-content-box'>
+              <div className='blog-body-content-left'>
+                <div className='blog-body-content-left-title'>
+                  东方战虎.辉(优质博客)
+                </div>
+                <div className='blog-body-content-left-content'>
+                  这是一篇博客这是一篇博客这是一篇博客这是一篇博客这是一篇博客这是一篇博客这是一篇博客这是一篇博客这是一篇博客这是一篇博客
+                </div>
+                <div className='blog-body-content-left-foot'>
+                  <span>2024-05-14</span>
+                  <span style={{ backgroundColor: "#555", padding: "2px 6px" }}>前端成长</span>
+                </div>
+              </div>
+              <div className='blog-body-content-right'>
+                <Image
+                  src={"/slot-bg1.jpg"}
+                  width={212}
+                  height={125}
+                  alt=''
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
       <footer style={{ position: "fixed", bottom: "20px", right: "5%", color: "#fff", textAlign: "center" }}>
         <p>友情链接</p>
         <a href="https://www.wujunhui.com/" target="_blank">东方战虎.辉(优质博客)</a>
